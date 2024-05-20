30b0c77786be6f68e7ba2abdd7cf132414fb3a92
KeenanKistoo <kistook19@gmail.com>
2024-05-20 17:01:24 +0200

    Added the plus btn

--- sbc-webapp/src/Components/card-comp/Card.jsx
+++ sbc-webapp/src/Components/card-comp/Card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import './Card.css';
 import CardImage from '../../Images/blank_card.png';
+import ImageIcon from '../../Images/plus_icon.png';
 
 function Card() {
+    let value = 1;
     return (
         <>
             <section className="card-input">
-                <img className="card" src={CardImage} alt="Card" />
-                <input className="input" type="text" placeholder="0"/>
+                <img className="card" src={CardImage} alt="Input Card Template"  style={{ gridColumn: value }}/> {/**Use of inline styling for */}
+                <input className="input" type="text" placeholder="0" style={{ gridColumn: value }} />{/**positioning purposes. */}
+                <button className="add-btn" style={{ gridColumn: value + 1 }}>+</button>
             </section>
         </>
     );
